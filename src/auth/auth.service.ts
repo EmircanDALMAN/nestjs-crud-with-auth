0a69751c3cc5e69b7619e5bd0f2c4b57dc592e88
Emircan Dalman <75789956+EmircanDALMAN@users.noreply.github.com>
2022-03-13 03:53:03 +0300

    Finish!

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,10 +3,16 @@ import { PrismaService } from '../prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
 
   async signin(dto: AuthDto) {
     // find the user by email
@@ -26,31 +32,50 @@ export class AuthService {
     if (!pwMatches) throw new ForbiddenException('Credentials incorrect');
 
     // send back the user
-    delete user.hash
-    return user
+    delete user.hash;
+    return this.signToken(user.id, user.email);
   }
 
   async signup(dto: AuthDto) {
-    try {
-      // generate password
-      const hash = await argon.hash(dto.password);
-      // save the new ser in the db
-      const user = await this.prisma.user.create({
-        data: {
-          email: dto.email,
-          hash,
-        },
-      });
+    // find the user by email
+    const oldUser = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
 
-      // remove hash password and return the saved user
-      delete user.hash;
-      return user;
-    } catch (e) {
-      if (e instanceof PrismaClientKnownRequestError) {
-        if (e.code === 'P2002')
-          throw new ForbiddenException('Credentials Taken');
-      }
-      throw e;
-    }
+    // if user exists throw exception because email is unique field
+    if (oldUser) throw new ForbiddenException('Credentials Taken');
+
+    // generate password
+    const hash = await argon.hash(dto.password);
+    // save the new ser in the db
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash,
+      },
+    });
+
+    // remove hash password and return the saved user
+    delete user.hash;
+    return this.signToken(user.id, user.email);
+  }
+
+  async signToken(
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string }> {
+    const payload = {
+      sub: userId,
+      email,
+    };
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '15m', // 15 minutes
+      secret: this.config.get('JWT_SECRET'),
+    });
+    return {
+      access_token: token,
+    };
   }
 }
---
+++ src/user/user.controller.ts
@@ -0,0 +1,24 @@
+import { Body, Controller, Get, Patch, Req, UseGuards } from "@nestjs/common";
+import { Request } from "express";
+import { JwtGuard } from "../auth/guard";
+import { GetUser } from "../auth/decorator";
+import { User } from "@prisma/client";
+import { EditUserDto } from "./dto";
+import { UserService } from "./user.service";
+
+@UseGuards(JwtGuard)
+@Controller("users")
+export class UserController {
+  constructor(private userService: UserService) {
+  }
+
+  @Get("me")
+  getMe(@GetUser() user: User) {
+    return user;
+  }
+
+  @Patch()
+  edit(@Body() dto: EditUserDto, @GetUser("id") userId: number) {
+    return this.userService.edit(userId, dto)
+  }
+}
